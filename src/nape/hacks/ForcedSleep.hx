0fa56cc76f592d193a4973399580d1c6e9a36f1e
Luca Deltodesco <luca@deltaluca.me.uk>
2012-07-29 21:40:09 +0100

    Inline docs added and changes made matching the equivalent inline_docs branch of nape core.

--- src/nape/hacks/ForcedSleep.hx
+++ src/nape/hacks/ForcedSleep.hx
@@ -12,7 +12,31 @@ import nape.space.Space;
 	import zpp_nape.dynamics.Arbiter.ZPP_Arbiter;
 #end
 
+/**
+ * Methods for manual sleeping and insertion of sleeping objects to a Space.
+ * <br/><br/>
+ * Haxe users can make use of the 'using' keyword to permit usage like:
+ * <pre>
+ * body.sleepBody();
+ * space.addSleepingBody(body);
+ * </pre>
+ * As opposed to the more verbose:
+ * <pre>
+ * ForcedSleep.sleepBody(body);
+ * ForcedSleep.addSleepingBody(space, body);
+ * </pre>
+ *
+ * @requires nape-hacks module.
+ */
 @:keep class ForcedSleep {
+    /**
+     * Force Body to go to sleep.
+     * <br/><br/>
+     * This method will not put to sleep anything that the Body is touching or connected to.
+     *
+     * @param body The Body to put to sleep.
+     * @throws # If body is null, or is not in a Space.
+     */
 	public static function sleepBody(body:Body) {
 		if(body==null) throw "Error: Can't force sleep null body";
 		if(body.space==null) throw "Error: This hack requires the Body to already be in a Space!";
@@ -55,6 +79,14 @@ import nape.space.Space;
 			for(s in body.shapes) b.space.bphase.sync(s.zpp_inner);
 	}
 
+    /**
+     * Force Constraint to go to sleep.
+     * <br/><br/>
+     * This method will not put to sleep anything that the Constraint is touching or connected to.
+     *
+     * @param constraint The Constraint to put to sleep.
+     * @throws # If constraint is null, or is not in a Space.
+     */
 	public static function sleepConstraint(constraint:Constraint) {
 		if(constraint==null) throw "Error: Can't force sleep null constraint";
 		if(constraint.space==null) throw "Error: This hack requires the Constraint to already be in a Space";
@@ -67,7 +99,32 @@ import nape.space.Space;
 		c.space.live_constraints.remove(c);
 	}
 
-	//sleep this body, and all bodies connected via constraints + those constraints
+    /**
+     * Force Compound to go to sleep.
+     * <br/><br/>
+     * This method will not put to sleep anything that the Compound's constituents are touching or connected to.
+     *
+     * @param compound The Compound to put to sleep.
+     * @throws # If compound is null, or is not in a Space.
+     */
+	public static function sleepCompound(compound:Compound) {
+		if(compound==null) throw "Error: Can't force sleep null compound";
+		if(compound.space==null) throw "Error: This hack requires the Compound to already be in a Space";
+		if(compound.space.zpp_inner.midstep) throw "Error: Even this hack can't operate from within space.step()!";
+
+        compound.visitBodies(sleepBody);
+		compound.visitConstraints(sleepConstraint);
+	}
+
+    /**
+     * Force Body, and all Bodies connected to go to sleep.
+     * <br/><br/>
+     * This method will traverse constraints visible via body.constraints list and put this Body
+     * as well as all Bodies it is directly or indirectly connected to; to sleep.
+     *
+     * @param body The Body to seed search for manual sleeping.
+     * @throws # If body is null, or is not in a Space.
+     */
 	public static function sleepConnected(body:Body) {
 		if(body==null) throw "Error: Can't force sleep connected from null Body";
 		if(body.space==null) throw "Error: This hack requires the Body to already be in a Space";
@@ -86,14 +143,27 @@ import nape.space.Space;
 		for(b in set) {
 			if(b.space==null) throw "Error: This hack requires Body's to already be in a Space regarding body found during sleepConnected";
 
-			if(b!=b.space.world) 
+			if(b!=b.space.world)
 				sleepBody(b);
 
 			for(c in b.constraints) sleepConstraint(c);
 		}
 	}
 
-	//should be used in preference to adding, then sleeping
+    /**
+     * Add a sleeping Body to a Space.
+     * <br/><br/>
+     * This method should be used in preference to inserting a Body, and then
+     * manually putting it to sleep.
+     * <br/><br/>
+     * If the Body is already in the Space, then this method will simply put it
+     * to sleep. If this Body is in a different Space, then calling this method
+     * will first remove the Body from its existing Space.
+     *
+     * @param space The Space to add Body to.
+     * @param body The Body to insert as a sleeping Body.
+     * @throws # If space is null, or body is null.
+     */
 	public static function addSleepingBody(space:Space, body:Body) {
 		if(body==null) throw "Error: Cannot add null body to Space";
 		if(space==null) throw "Error: Cannot add body to null Space";
@@ -133,8 +203,8 @@ import nape.space.Space;
 			b.validate_gravMass();
 			b.validate_worldCOM();
 			b.validate_axis();
-		
-			for(shape in body.shapes) {	
+
+			for(shape in body.shapes) {
 				var s = shape.zpp_inner;
 				if(s.isPolygon())
 					s.polygon.validate_gaxi();
@@ -142,7 +212,20 @@ import nape.space.Space;
 		}
 	}
 
-	//should be used in preference to adding, then sleeping
+    /**
+     * Add a sleeping Constraint to a Space.
+     * <br/><br/>
+     * This method should be used in preference to inserting a Constraint, and then
+     * manually putting it to sleep.
+     * <br/><br/>
+     * If the Constraint is already in the Space, then this method will simply put it
+     * to sleep. If this Constraint is in a different Space, then calling this method
+     * will first remove the Constraint from its existing Space.
+     *
+     * @param space The Space to add Constraint to.
+     * @param constraint The Constraint to insert as a sleeping Constraint.
+     * @throws # If space is null, or constraint is null.
+     */
 	public static function addSleepingConstraint(space:Space, constraint:Constraint) {
 		if(constraint==null) throw "Error: Cannot add null constraint to Space";
 		if(space==null) throw "Error: Cannot add constraint to null Space";
@@ -165,7 +248,20 @@ import nape.space.Space;
 		s.constraints.add(c);
 	}
 
-	//should be used in preference to adding, then sleeping
+    /**
+     * Add a sleeping Compound to a Space.
+     * <br/><br/>
+     * This method should be used in preference to inserting the Compound, and then
+     * manually putting its constituents to sleep.
+     * <br/><br/>
+     * If the Compound is already in the Space, then this method will simply put it
+     * to sleep. If this Compound is in a different Space, then calling this method
+     * will first remove the Compound from its existing Space.
+     *
+     * @param space The Space to add Compound to.
+     * @param constraint The Compound to insert as a sleeping Compound.
+     * @throws # If space is null, or compound is null.
+     */
 	public static function addSleepingCompound(space:Space, compound:Compound) {
 		if(compound==null) throw "Error: Cannot add null compound to Space";
 		if(space==null) throw "Error: Cannot add compound to null Space";
